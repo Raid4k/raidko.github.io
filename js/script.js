58135e21083555addd69f78992e1e748a0884810
raid4k <raid4k@gmail.com>
2019-08-13 18:37:07 +0300

    hideshow

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function () {
-    
+    jQuery(".collapse_btn").on("click", function () {
+        jQuery(this).prev().is(":visible") ? (jQuery(this).text("Show"), jQuery(this).prev().hide("slow")) : (jQuery(this).text("Collapse"), jQuery(this).prev().show("slow"))
+    });
 });
