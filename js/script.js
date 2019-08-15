6eada07a5a24543d6bc7355c6150555667675116
raid4k <raid4k@gmail.com>
2019-08-15 17:42:47 +0300

    anchors

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@ jQuery(document).ready(function () {
     jQuery(window).scroll(function () {
         jQuery(this).scrollTop() > 1 ? jQuery(".anchor-header").addClass("fixed") : jQuery(".anchor-header").removeClass("fixed")
     });
-});
+    jQuery("#menu").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
 
+        //забираем идентификатор бока с атрибута href
+        var id  = jQuery(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = jQuery(id).offset().top;
+        
+        //анимируем переход на расстояние - top за 1500 мс
+        jQuery('body,html').animate({scrollTop: top - 90}, 1000);
+        return;
+    });
+
+});
