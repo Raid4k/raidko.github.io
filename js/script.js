6017077a4e2df705eaacd51358af9e9e3b933adc
raid4k <raid4k@gmail.com>
2019-08-15 16:43:24 +0300

    archonheader

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ jQuery(document).ready(function () {
     jQuery(".collapse_btn").on("click", function () {
         jQuery(this).prev().is(":visible") ? (jQuery(this).text("Show"), jQuery(this).prev().hide("slow")) : (jQuery(this).text("Collapse"), jQuery(this).prev().show("slow"))
     });
+    jQuery(window).scroll(function () {
+        jQuery(this).scrollTop() > 1 ? jQuery(".anchor-header").addClass("fixed") : jQuery(".anchor-header").removeClass("fixed")
+    });
 });
+
